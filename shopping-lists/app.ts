f8033da5aa8bb2c98bc36fdb1d664f743c5bd736
Ojakoo <ilari.ojakorpi@gmail.com>
2022-09-09 18:01:49 +0300

    basic routing

--- shopping-lists/app.ts
+++ shopping-lists/app.ts
@@ -1,8 +1,22 @@
 import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
+import { configure, renderFile } from "https://deno.land/x/eta@v1.12.3/mod.ts";
+
+configure({
+  views: `${Deno.cwd()}/views`,
+});
+
+const responseDetails = {
+  headers: { "Content-Type": "text/html;charset=UTF-8" },
+};
 
 const handleRequest = async (request) => {
-  console.log("Responding with Hello world!");
-  return new Response("Hello world!");
+  const url = new URL(request.url);
+
+  if (url.pathname === "/lists") {
+    return new Response(await renderFile("lists.eta", {}), responseDetails);
+  } else {
+    return new Response("404", { status: 404 });
+  }
 };
 
 serve(handleRequest, { port: 7777 });
